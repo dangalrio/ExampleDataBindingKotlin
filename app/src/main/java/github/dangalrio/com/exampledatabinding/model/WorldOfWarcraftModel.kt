0b2ef387465095ce70fa47d2e41b880b706156f0
daniel.gallardo <daniel.gallardo@s-dos.es>
2018-06-23 13:23:22 +0200

    Creacción layout vista principal y configuración de gradle

---
+++ app/src/main/java/github/dangalrio/com/exampledatabinding/model/WorldOfWarcraftModel.kt
@@ -0,0 +1,8 @@
+package github.dangalrio.com.exampledatabinding.model
+
+import java.io.Serializable
+
+class WorldOfWarcraftModel ( var id : Int,
+                             var name: String,
+                             var description: String,
+                             var imageUrl: String) : Serializable
